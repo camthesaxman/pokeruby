1b0fff955164d79cd76cd1ede0a3d75e59610c16
camthesaxman <cameronghall@cox.net>
2016-10-13 15:30:03 -0500

    more decompilation

--- src/item_effect.c
+++ src/item_effect.c
@@ -12,10 +12,21 @@ struct UnknownStruct2 {
     u8 unknown[12];
 };
 
+extern void dp01_build_cmdbuf_x00_a_b_0(u8, u8, u8);
+extern void dp01_battle_side_mark_buffer_for_execution(u8);
+extern u8 pokemon_order_func(u8);
 extern u8 battle_side_get_owner(u8);
 extern u8 itemid_get_x12(u16);
 extern void sub_8111924(struct Pokemon *, u16, u8, u8);
 
+extern struct UnknownStruct gUnknown_02024CA8[];
+extern u32 gUnknown_02024BEC;
+extern u8 gUnknown_030042E0[]; //Not sure if this is a struct or array
+extern u8 gUnknown_02024C0C;
+extern u32 gBitTable[];
+extern u8 gUnknown_082082F2[];
+extern u8 gUnknown_08208240[];
+extern u8 gUnknown_08208238[];
 extern u32 gExperienceTables[8][101];
 extern struct BaseStats gBaseStats[];
 extern struct UnknownStruct2 gUnknown_02024C80[];
@@ -32,6 +43,8 @@ extern struct BattlePokemon gBattleMons[];
 
 u32 sub_803E1B0(struct Pokemon *, u16 b, u8 c, u8 d, u8 e);
 extern bool8 HealStatusConditions(struct Pokemon *pkmn, u32 unused, u32 a, u8 b);
+extern u16 sub_8040020(struct Pokemon *pkmn);
+extern u16 GetEvolutionTargetSpecies(struct Pokemon *, u8, u16);
 
 u8 ExecuteTableBasedItemEffect_(struct Pokemon *pkmn, u16 b, u8 c, u8 d)
 {
@@ -43,9 +56,12 @@ u32 sub_803E1B0(struct Pokemon *pkmn, u16 b, u8 c, u8 d, u8 e)
     struct UnknownStruct *sp20;
     u8 sp30;
     u8 sp18;
-    u32 sp1C;
+    u32 sp28;
+    u32 sp38;
     u16 r4var1;
-    
+    u32 sp1C = 1;
+    u8 sp24 = 6;
+    u32 sp2C = 0;
     u8 sp34 = 4;
     u16 heldItem = GetMonData(pkmn, MON_DATA_HELD_ITEM, NULL);
     if(heldItem == 0xAF)
@@ -254,10 +270,343 @@ u32 sub_803E1B0(struct Pokemon *pkmn, u16 b, u8 c, u8 d, u8 e)
                         sp1C = 0;
                 }
                 //_0803E702
-                //Continue decompiling here
+                if(!(sp20->unknown[sp18] & 0x1))
+                    continue;
+                if(!gMain.unk1)
+                    continue;
+                if(sp34 == 4)
+                    continue;
+                if(!(gBattleMons[sp34].status2 & 0x7))
+                    continue;
+                gBattleMons[sp34].status2 &= 0xFFFFFFF8;
+                sp1C = 0;
+                continue;
                 break;
             case 4: //_0803E77C
+            {
+                u32 r10;
+                
+                r10 = sp20->unknown[sp18];
+                if(sp20->unknown[sp18] & 0x20)
+                {
+                    u32 sp0;
+                    u16 move;
+                    
+                    r10 &= 0xDF;
+                    
+                    sp0 = (GetMonData(pkmn, MON_DATA_PP_BONUSES, NULL) & gUnknown_08208238[d]) >> (d * 2);
+                    move = GetMonData(pkmn, MON_DATA_MOVE1 + d, NULL);
+                    sp28 = CalculatePPWithBonus(move, GetMonData(pkmn, MON_DATA_PP_BONUSES, NULL), d);
+                    
+                    if(sp0 <= 2 && sp28 > 4)
+                    {
+                        sp0 = GetMonData(pkmn, MON_DATA_PP_BONUSES, NULL) + gUnknown_08208240[d];
+                        SetMonData(pkmn, MON_DATA_PP_BONUSES, &sp0);
+                        sp0 = CalculatePPWithBonus(GetMonData(pkmn, MON_DATA_MOVE1 + d, NULL), sp0, d) - sp28;
+                        sp0 += GetMonData(pkmn, MON_DATA_PP1 + d, NULL);
+                        SetMonData(pkmn, MON_DATA_PP1 + d, &sp0);
+                        sp1C = 0;
+                    }
+                }
+                //_0803E854
+                sp28 = 0;
+                if(!r10)
+                    continue;
+                //_0803E862
+              _0803E862:
+                if((sp20->unknown[sp18] & 0xDF) & 0x1)
+                {
+                    //_0803E876
+                    switch(sp28)
+                    {
+                        case 0:
+                        case 1:
+                        //_0803E8AC
+                        {
+                            u32 sp0;
+                            u16 r5 = sub_8040020(pkmn);
+                            u32 r4;
+                            
+                            if(r5 > 0x1FD)
+                                return 1;
+                            sp0 = GetMonData(pkmn, gUnknown_082082F2[sp28], NULL);
+                            if(sp0 > 0x63)
+                                break;
+                            //I probably made a mistake here
+                            //This asm code is very awkward and tricky
+                            if(sp0 + sp20->unknown[sp24] > 0x64)
+                                r4 = sp20->unknown[sp24] + 0x64 - (sp0 + sp20->unknown[sp24]);
+                            else
+                                r4 = sp20->unknown[sp24];
+                            //_0803E8F6
+                            if(r5 + r4 > 0x1FE)
+                                r4 = r4 - 0x1FE - (r5 + r4);
+                            
+                            SetMonData(pkmn, gUnknown_082082F2[sp28], &sp0);
+                            CalculateMonStats(pkmn);
+                            sp24++;
+                            break;
+                        }
+                        case 2:
+                        //_0803E934
+                        {
+                            u8 sp0;
+                            
+                            if(r10 & 0x10)
+                            {
+                                if(GetMonData(pkmn, MON_DATA_HP, NULL))
+                                {
+                                    //from _0803EC9E (cross-jump optimization)
+                                    sp24++;
+                                    break;
+                                }
+                                //_0803E954
+                                if(gMain.unk1)
+                                {
+                                    if(sp34 != 4)
+                                    {
+                                        gUnknown_02024C0C &= ~gBitTable[sp34];
+                                        CopyPlayerPartyMonToBattleData(sp34, pokemon_order_func(gUnknown_02024A6A[sp34]));
+                                    }
+                                    else
+                                    {
+                                        //_0803E9B4
+                                        gUnknown_02024C0C &= ~gBitTable[gUnknown_02024C0C ^ 2];
+                                    }
+                                    //_0803E9CC
+                                    if(battle_side_get_owner(gUnknown_02024A60) == 0 &&
+                                    gUnknown_030042E0[4] <= 0xFE)
+                                    {
+                                        gUnknown_030042E0[4]++;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                //_0803E9F4
+                                if(GetMonData(pkmn, MON_DATA_HP, NULL))
+                                {
+                                    //from _0803EC9E (cross-jump optimization)
+                                    sp24++;
+                                    break;
+                                }
+                            }
+                            //_0803EA0A
+                            sp0 = sp20->unknown[sp24++];
+                            switch(sp0)
+                            {
+                                case 0xFF:
+                                    sp0 = GetMonData(pkmn, MON_DATA_MAX_HP, NULL) - GetMonData(pkmn, MON_DATA_HP, NULL);
+                                    break;
+                                case 0xFE:
+                                    sp0 = GetMonData(pkmn, MON_DATA_MAX_HP, NULL) / 2;
+                                    if(sp0 == 0)
+                                        sp0 = 1;
+                                    break;
+                                case 0xFD:
+                                    sp0 = *(u8 *)0x020160FA;
+                                    break;
+                            }
+                            //_0803EA6C
+                            if(GetMonData(pkmn, MON_DATA_MAX_HP, NULL) != GetMonData(pkmn, MON_DATA_HP, NULL))
+                            {
+                                if(e == 0)
+                                {
+                                    sp0 += GetMonData(pkmn, MON_DATA_HP, NULL);
+                                    if(sp0 > GetMonData(pkmn, MON_DATA_MAX_HP, NULL))
+                                        sp0 = GetMonData(pkmn, MON_DATA_MAX_HP, NULL);
+                                    //_0803EAB8
+                                    SetMonData(pkmn, MON_DATA_HP, &sp0);
+                                    if(gMain.unk1 && sp34 != 4)
+                                    {
+                                        gBattleMons[sp34].hp = sp0;
+                                        if(!(r10 & 0x10) && !battle_side_get_owner(gUnknown_02024A60))
+                                        {
+                                            u8 r5;
+                                            
+                                            if(gUnknown_030042E0[3] <= 0xFE)
+                                                gUnknown_030042E0[3]++;
+                                            //_0803EB08
+                                            r5 = gUnknown_02024A60;
+                                            gUnknown_02024A60 = sp34;
+                                            dp01_build_cmdbuf_x00_a_b_0(0, 0, 0);
+                                            dp01_battle_side_mark_buffer_for_execution(gUnknown_02024A60);
+                                            gUnknown_02024A60 = r5;
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    //_0803EB40
+                                    gUnknown_02024BEC = -sp0;
+                                }
+                                //_0803EB48
+                                sp1C = 0;
+                            }
+                            //_0803EB4C
+                            r10 &= 0xEF;
+                            break;
+                        }
+                        case 3:
+                        //_0803EB5C
+                        {
+                            u8 sp0;
+                            
+                            if(r10 & 0x2)
+                            {
+                                int i = 0;  //r5
+                                
+                                sp38 = sp24 + 1;
+                                
+                                //_0803EB70
+                                for(; i < 4; i++)
+                                {
+                                    sp0 = GetMonData(pkmn, MON_DATA_PP1 + i, NULL);
+                                    
+                                    if(CalculatePPWithBonus(
+                                     GetMonData(pkmn, MON_DATA_MOVE1 + i, NULL),
+                                     GetMonData(pkmn, MON_DATA_PP_BONUSES, NULL),
+                                     i) == sp0)
+                                        continue;
+                                    sp0 += sp20->unknown[sp24];
+                                    if(CalculatePPWithBonus(
+                                     GetMonData(pkmn, MON_DATA_MOVE1 + i, NULL),
+                                     GetMonData(pkmn, MON_DATA_PP_BONUSES, NULL),
+                                     i) > sp0)
+                                    {
+                                        sp0 = CalculatePPWithBonus(
+                                        GetMonData(pkmn, MON_DATA_MOVE1 + i, NULL),
+                                        GetMonData(pkmn, MON_DATA_PP_BONUSES, NULL),
+                                        i);
+                                    }
+                                    //_0803EC28
+                                    SetMonData(pkmn, MON_DATA_PP1, &sp0);
+                                    if(gMain.unk1 && sp34 != 4 &&
+                                    !(gBattleMons[sp34].status2 & 0x200000) &&
+                                    !(gUnknown_02024CA8[sp34].unknown[0x18] & gBitTable[i]))
+                                    {
+                                        gBattleMons[sp34].pp[i] = sp0;
+                                    }
+                                    //_0803EC8E
+                                    sp1C = 0;
+                                }
+                                //_0803EC9A
+                                sp24 = sp38;
+                                break;
+                            }
+                            //_0803ECB8
+                            sp0 = GetMonData(pkmn, MON_DATA_PP1 + d, NULL);
+                            if(CalculatePPWithBonus(
+                            GetMonData(pkmn, MON_DATA_MOVE1 + d, NULL),
+                            GetMonData(pkmn, MON_DATA_PP_BONUSES, NULL),
+                            d) == d)
+                                break;
+                            //_0803ED00
+                            sp24++;
+                            sp0 += sp20->unknown[sp24];
+                            if(CalculatePPWithBonus(
+                            GetMonData(pkmn, MON_DATA_MOVE1 + d, NULL),
+                            GetMonData(pkmn, MON_DATA_PP_BONUSES, NULL),
+                            d) < d)
+                            {
+                                sp0 = CalculatePPWithBonus(
+                                 GetMonData(pkmn, MON_DATA_MOVE1 + d, NULL),
+                                 GetMonData(pkmn, MON_DATA_PP_BONUSES, NULL),
+                                 d);
+                            }
+                            //_0803ED74
+                            SetMonData(pkmn, MON_DATA_PP1 + d, &sp0);
+                            if(gMain.unk1 && sp34 != 4 &&
+                            !(gBattleMons[sp34].status2 & 0x200000) &&
+                            !((gUnknown_02024CA8[sp34].unknown[0x18] >> 4) & gBitTable[d]))
+                                gBattleMons[sp34].pp[d] = sp0;
+                            //_0803EDD8
+                            sp1C = 0;
+                            break;
+                        }
+                        case 7:
+                        //_0803EDF4
+                        {
+                            u16 evolution = GetEvolutionTargetSpecies(pkmn, 2, b);
+                            
+                            if(evolution)
+                            {
+                                //jump to _0803E35C
+                                sub_8111924(pkmn, evolution, 0, spC);
+                                return 0;
+                            } 
+                            break;
+                        }
+                    }
+                }
+                //_0803EE0A
+                if((r10 >> 1) == 0)
+                    continue;
+                else
+                    goto _0803E862; //must be a loop?
+            }
             case 5: //_0803EE1E
+            {
+                u8 r10 = sp20->unknown[sp18];
+                
+                sp28 = 0;
+                if(!r10)
+                    continue;
+              _0803EE32:
+                if(r10 & 1)
+                {
+                    switch(sp28)
+                    {
+                        case 0:
+                        case 1:
+                        case 2:
+                        case 3:
+                        {
+                            //_0803EE74
+                            u16 r5 = sub_8040020(pkmn);
+                            u32 r4;
+                            u32 sp0;
+                            
+                            if(r5 > 509)
+                                return 1;
+                            //_0803EE88
+                            sp0 = GetMonData(pkmn, gUnknown_082082F2[sp28 + 2], NULL);
+                            if(sp0 > 99)
+                                break;
+                            //_0803EEA6
+                            if(sp0 + sp20->unknown[sp24] > 100)
+                                r4 = sp20->unknown[sp24] + 100 - (sp0 + sp20->unknown[sp24]);
+                            else
+                                r4 = sp20->unknown[sp24];
+                            if(r5 + r4 > 0x1FE)
+                                r4 = (r4 + 0x1FE) - (r5 + r4);
+                            SetMonData(pkmn, gUnknown_082082F2[sp28 + 2], &sp0);
+                            CalculateMonStats(pkmn);
+                            sp1C = 0;
+                            //goto _0803F130
+                            sp24++;
+                            break;
+                        }
+                        case 4:
+                        {
+                            //_0803EEF8
+                            u32 sp0;
+                            
+                            break;
+                        }
+                        case 5:
+                            //_0803EFCC
+                            break;
+                        case 6:
+                            //_0803F026
+                            break;
+                        case 7:
+                            //_0803F07C
+                            break;
+                    }
+                }
+                //_0803F13A
+            }
         }
         
       loop_end:
